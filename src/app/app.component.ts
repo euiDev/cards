febc89cea78921d8335791f6a2eeabe1993ebf4b
Eugene Angelo <dev.eugene.paje@gmail.com>
2020-10-13 20:41:05 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat tree',
+      imageUrl: 'assets/card_images/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy mountain',
+      imageUrl: 'assets/card_images/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of snowy mountain'
+    },
+    {
+      title: 'Mountain biking',
+      imageUrl: 'assets/card_images/biking.jpeg',
+      username: 'biking12222',
+      content: 'I did some biking today'
+    }
+  ];
 }
